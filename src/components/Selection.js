122fd79d5c3f0b41ba35db875eaff3e1bfa9da03
Satyam <nikamsatyam187@gmail.com>
2025-12-04 10:43:20 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React from 'react';
 
+function Selection({ applyColor }) {
+  const colors = ['red', 'green', 'blue'];
+
+  return (
+    <div>
+      {colors.map((color) => (
+        <button
+          key={color}
+          onClick={() => applyColor(color)}
+          style={{ background: color, margin: '5px' }}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Selection;
