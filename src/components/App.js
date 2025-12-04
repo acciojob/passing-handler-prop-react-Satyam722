e03267ccc1862b1eb828b6a8bb48ee31f78eaaa0
Satyam <nikamsatyam187@gmail.com>
2025-12-04 10:42:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,27 @@
-import React, { useState } from "react";
-import '../styles/App.css';
+import React, { useState } from 'react';
 import Selection from './Selection';
-import ColourSelector from './ColourSelector';
 
-const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
+function App() {
+  const [selectedColor, setSelectedColor] = useState('white');
 
-const title = 'Select the gradient and then the Box to change the color';
-
-const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
-  const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
+  const handleColorChange = (color) => {
+    setSelectedColor(color);
+  };
 
   return (
-    <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
-
-      <div className="row">
-        {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
-        ))}
+    <div>
+      <div className="fix-box" style={{ background: selectedColor }}>
+        Box 1
       </div>
-
-      <div className='row' id="children-wrapper">
-        {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
-          ))
-        }
+      <div className="fix-box" style={{ background: selectedColor }}>
+        Box 2
       </div>
-    </div >
-  )
+      <div className="fix-box" style={{ background: selectedColor }}>
+        Box 3
+      </div>
+      <Selection applyColor={handleColorChange} />
+    </div>
+  );
 }
-
 
 export default App;
