e3b374cc171c19caf1175d96ea1095b9044ffe39
Satyam <nikamsatyam187@gmail.com>
2025-12-04 16:56:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,36 @@
-import React, { useState } from 'react';
-import ColourSelector from './ColourSelector';
-import Selection from './Selection';
+import React, { useState } from "react";
+import ColourSelector from "./ColourSelector";
+import Selection from "./Selection";
 
-function App() {
-  const [nextBackground, setNextBackground] = useState({ background: '' });
+export default function App() {
+  const [selectedColor, setSelectedColor] = useState("");
 
-  // Handler function passed as prop
-  const selectNextBackground = (background) => {
-    setNextBackground(background);
-  };
-
-  // Color configuration
-  const colourConfig = [
-    {
-      key: 'blue',
-      label: 'Blue',
-      classname: 'btn-blue',
-      background: 'rgb(34, 193, 195)'
-    },
-    {
-      key: 'orange',
-      label: 'Orange', 
-      classname: 'btn-orange',
-      background: 'rgb(221, 112, 18)'
-    },
-    {
-      key: 'green',
-      label: 'Green',
-      classname: 'btn-green',
-      background: 'rgb(44, 209, 88)'
-    }
+  const config = [
+    { label: "Blue", value: "blue" },
+    { label: "Orange", value: "orange" },
+    { label: "Green", value: "green" }
   ];
 
   return (
-    <div id="master">
-      <h5 className="heading">Color Picker</h5>
-      
-      {/* Buttons for color selection */}
-      <div className="row">
-        {colourConfig.map((config) => (
-          <ColourSelector 
-            key={config.key}
-            config={config}
-            selectNextBackground={selectNextBackground}
+    <div className="App">
+      <h1>Must use Props and State. Help us to change the color!</h1>
+
+      <div className="button-cont">
+        {config.map((colObj) => (
+          <ColourSelector
+            key={colObj.value}
+            config={colObj}
+            handleClick={setSelectedColor}
           />
         ))}
       </div>
 
-      {/* Three boxes that change color */}
-      <div className="row" id="children-wrapper">
-        <Selection selection={nextBackground} />
-        <Selection selection={nextBackground} />
-        <Selection selection={nextBackground} />
+      <div className="selection-cont">
+        {/* Three Selection Boxes */}
+        <Selection selectedColor={selectedColor} />
+        <Selection selectedColor={selectedColor} />
+        <Selection selectedColor={selectedColor} />
       </div>
     </div>
   );
 }
-
-export default App;
