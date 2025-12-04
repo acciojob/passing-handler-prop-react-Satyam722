916b46878ad17e1278f67b018b188d8dbf649669
Satyam <nikamsatyam187@gmail.com>
2025-12-04 17:02:27 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Selection({ selectedColor }) {
+  const [boxColor, setBoxColor] = useState(""); // independent per box
+
   return (
     <div
       className="fix-box"
-      style={{ backgroundColor: selectedColor }}
+      onClick={() => setBoxColor(selectedColor)}  // apply active color
+      style={{ backgroundColor: boxColor }}
     >
       Selection
     </div>
