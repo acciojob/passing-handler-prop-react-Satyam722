131ec5fb67495e2af654a9539766afcf57efecdf
Satyam <nikamsatyam187@gmail.com>
2025-12-04 16:56:59 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
-const Selection = ({ selection }) => {
-  const [style, setStyle] = useState({});
-
-  // Update style when selection changes
-  useEffect(() => {
-    if (selection && selection.background) {
-      setStyle({ backgroundColor: selection.background });
-    }
-  }, [selection]);
-
+export default function Selection({ selectedColor }) {
   return (
-    <div 
+    <div
       className="fix-box"
-      style={style}
+      style={{ backgroundColor: selectedColor }}
     >
-      {/* Empty box that changes color */}
+      Selection
     </div>
   );
-};
-
-export default Selection;
+}
