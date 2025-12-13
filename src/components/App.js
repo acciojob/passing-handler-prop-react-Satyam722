dd70d2c553e0878aade963ade94f16113b6a5c61
Satyam <nikamsatyam187@gmail.com>
2025-12-13 19:52:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,56 @@
 import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
+import "../styles/App.css";
+import "../styles/Child.css";
+import Selection from "./Selection";
+import ColourSelector from "./ColourSelector";
 
 const colourConfig = [
   {
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
+    key: "blue",
+    label: "Blue",
+    classname: "btn-blue",
+    background: "rgb(34, 193, 195)",
   },
   {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
+    key: "orange",
+    label: "Orange",
+    classname: "btn-orange",
+    background: "rgb(221, 112, 18)",
   },
   {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
+    key: "green",
+    label: "Green",
+    classname: "btn-green",
+    background: "rgb(44, 209, 88)",
+  },
 ];
 
-const title = 'Select the gradient and then the Box to change the color';
+const title = "Select the gradient and then the Box to change the color";
 
 const App = () => {
-  const [nextBackground, selectNextBackground] = useState({ background: "" });
+  let [nextBackground, selectNextBackground] = useState({ background: "" });
+
+  function applyColor() {
+    return nextBackground?.background;
+  }
 
   return (
     <div id="master">
       <h5 className="heading">{title}</h5>
 
-      {/* Buttons */}
-      <div className="row">
-        {colourConfig.map((config) => (
-          <ColourSelector
-            key={config.key}
-            config={config}
-            selectNextBackground={selectNextBackground}
-          />
-        ))}
+      <div className="row holder">
+        {colourConfig.map(config =>{
+          return <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
+        })}
       </div>
 
-      {/* ✅ ALL boxes react to selected color */}
-      <div className="row" id="children-wrapper">
-        {["1", "2", "3"].map((id) => (
-          <Selection
-            key={id}
-            background={nextBackground}
-          />
+      <div className="row holder" id="children-wrapper">
+        {["selection1", "selection2", "selection3"].map((key) => (
+          <Selection key={key} applyColor={applyColor} />
         ))}
       </div>
     </div>
-  );
+  ); // prettier-ignore
 };
 
 export default App;
