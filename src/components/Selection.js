e07e776902b3a1047280b16ead74ce2956b2948f
Satyam <nikamsatyam187@gmail.com>
2025-12-13 19:53:46 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Selection = ({ background }) => {
+const Selection = (props) => {
+  const { applyColor } = props;
+
+  function setBackground(evt) {
+    const elem = evt.target;
+    elem.style.backgroundColor = applyColor();
+  }
+
   return (
-    <div
-      className="fix-box"
-      style={background}
-    >
-    </div>
-  );
+    <div className="fix-box subheading" onClick={setBackground}>Selection</div>
+  ); // prettier-ignore
 };
 
 export default Selection;
